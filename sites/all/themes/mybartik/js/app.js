e8a83ec22011c850091a9d31cb8fcecc2e2ded0f
Bozeak <bozeak@gmail.com>
2014-09-23 22:26:33 +0300

    Correcting book page carousel and some other features

--- sites/all/themes/mybartik/js/app.js
+++ sites/all/themes/mybartik/js/app.js
@@ -1,7 +1,7 @@
-jQuery(document).ready(function() {
-	jQuery('#block-views-exp-catalog-page-page h2').click(function() {
-	 	jQuery('#views-exposed-form-catalog-page-page').toggle('slow');
-	});
-});
+// jQuery(document).ready(function() {
+// 	jQuery('#block-views-exp-catalog-page-page h2').click(function() {
+// 	 	jQuery('#views-exposed-form-catalog-page-page').toggle('slow');
+// 	});
+// });
 
 
